d07b3bae7f5da3c01d68b9a47d8e037b33206c25
AgnosticSix <mateo_godfather@hotmail.com>
2025-01-20 23:40:28 -0600

    Solucionado error prop input name

--- src/components/ui/Input/Input.jsx
+++ src/components/ui/Input/Input.jsx
@@ -2,10 +2,11 @@ import PropTypes from "prop-types";
 import { InputText } from "primereact/inputtext";
 import "./input.css";
 
-export const Input = ({ id, value, placeholder, onChange, isReadOnly }) => {
+export const Input = ({ id, name, value, placeholder, onChange, isReadOnly }) => {
   return (
     <InputText
       id={id}
+      name={name}
       value={value}
       placeholder={placeholder}
       onChange={onChange}
@@ -17,6 +18,7 @@ export const Input = ({ id, value, placeholder, onChange, isReadOnly }) => {
 
 Input.propTypes = {
   id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   value: PropTypes.string,
   placeholder: PropTypes.string,
   onChange: PropTypes.func,
